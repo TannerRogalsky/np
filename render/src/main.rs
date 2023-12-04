d1692adfec86fdafc1f8ce535a3f85f571685219
Tanner Rogalsky <tanner@tannerrogalsky.com>
2023-12-04 15:01:38 +0000

    game done

--- render/src/main.rs
+++ render/src/main.rs
@@ -3,14 +3,7 @@ use render::*;
 #[tokio::main]
 async fn main() {
     let game_id = 4725907836895232;
-    let ticks = vec![
-        0, 6, 12, 18, 24, 30, 36, 42, 48, 54, 60, 66, 72, 78, 84, 90, 96, 102, 108, 114, 120, 126,
-        132, 138, 144, 150, 156, 162, 168, 174, 180, 186, 192, 198, 204, 210, 216, 222, 228, 234,
-        240, 246, 252, 258, 264, 270, 276, 282, 288, 294, 300, 306, 312, 318, 324, 330, 336, 342,
-        348, 354, 360, 366, 372, 378, 384, 390, 396, 402, 408, 414, 420, 426, 432, 438, 444, 450,
-        456, 462, 468, 474, 480, 486, 492, 498, 504, 510, 516, 522, 528, 534, 540, 546, 552, 558,
-        564, 570, 576, 582,
-    ];
+    let ticks = (0..=804).step_by(6);
     // let tick = game.most_recent_tick(&db).await.unwrap();
     // let tick = 6;
     for tick in ticks {
@@ -35,7 +28,7 @@ fn process(game_id: i64, tick: i64) {
     });
 
     let bounds = match bounds {
-        AABB::Unset => panic!("unset bounds"),
+        AABB::Unset => return,
         AABB::Set(bounds) => bounds,
     };
 
@@ -198,7 +191,8 @@ fn process(game_id: i64, tick: i64) {
     document = document.add(star_titles);
 
     svg::save(
-        root.join("svgs")
+        root.join("docs")
+            .join("svgs")
             .join(format!("{}_tick{:0>5}.svg", game_id, tick)),
         &document,
     )
